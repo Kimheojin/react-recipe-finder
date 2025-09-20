94583ec5a25f8e1f1b854c8db0656c78b611df6c
Jin <hurjin1109@naver.com>
2025-09-20 15:19:02 +0900

    INIT : react 초기화 - readme, css 등 초기화

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>초기 설저 test</div>
+      <div>초기 설저 test</div>
+      <div>초기 설저 test</div>
+    </>
+  );
+}
+
+export default App;
