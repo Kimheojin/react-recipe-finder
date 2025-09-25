ae82d8deeb20f225402476f4c01e3446f7a4d3bd
Jin <hurjin1109@naver.com>
2025-09-25 16:06:15 +0900

    FEAT : proxy 서버를 통한 target 설정 (ngnix 향하게)

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port: 1000,
+    proxy: {
+      "/seo": {
+        target: "http://heojineee.ddnsking.com:1400",
+        changeOrigin: true,
+      },
+    },
+  },
+});
