6d1ea0a22d654de16a879b3b62089880166f04f4
Jin <hurjin1109@naver.com>
2025-09-25 17:52:18 +0900

    FEAT : class-transformer 관련 라이브러리 추가 및 httpclient 관련 설정 코드 추가

--- src/repository/http/HttpRepository.ts
+++ src/repository/http/HttpRepository.ts
@@ -1,6 +1,23 @@
-
+import { inject, singleton } from "tsyringe";
+import AxiosHttpClient, {
+  type HttpRequestConfig,
+} from "../../http/AxiosHttpClient";
+import { type ClassConstructor, plainToInstance } from "class-transformer";
 
 @singleton()
-export default HttpRepository(
-    
-)
+export default class HttpRepository {
+  constructor(
+    @inject(AxiosHttpClient)
+    private readonly httpClient: AxiosHttpClient
+  ) {}
+
+  // clazz -> 반환 관련 클래스 생성자
+  public get<T>(
+    config: HttpRequestConfig,
+    clazz: ClassConstructor<T>
+  ): Promise<T> {
+    return this.httpClient
+      .request({ ...config, method: "GET" })
+      .then((response) => plainToInstance(clazz, response));
+  }
+}
