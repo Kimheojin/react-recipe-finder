023dc381cfbf55eaa1e2744628e1be4f14f1862a
Jin <hurjin1109@naver.com>
2025-09-27 15:54:04 +0900

    FEAT : chakraUI 관련 설정 추가 + 기본 글꼴 적용

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,23 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
+import { ChakraProvider, createSystem, defaultConfig } from "@chakra-ui/react";
+
+const system = createSystem(defaultConfig, {
+    theme: {
+        tokens: {
+            fonts: {
+                body: { value: '"MaruBuri", Arial, sans-serif' },
+                heading: { value: '"MaruBuri", Arial, sans-serif' },
+            },
+        },
+    },
+});
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+    <StrictMode>
+        <ChakraProvider value={system}>
+            <App />
+        </ChakraProvider>
+    </StrictMode>
 );
--- src/views/HomeView.tsx
+++ src/views/HomeView.tsx
@@ -1,7 +1,10 @@
+import SearchInput from "../components/SearchInput";
+
 export default function HomeView() {
     return (
         <>
             <h1>HomeView + 라우터 부분</h1>
+            <SearchInput />
         </>
     );
 }
