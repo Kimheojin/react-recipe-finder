180d9613ccff042976a0dad77d6867fba0af559b
Jin <hurjin1109@naver.com>
2025-10-02 16:06:02 +0900

    FEAT : api 스펙 변경에 따른 로직 수정

--- src/entity/integratedSearch/response/ListSearchRecipeResponse.ts
+++ src/entity/integratedSearch/response/ListSearchRecipeResponse.ts
@@ -1,5 +1,9 @@
 import type SingleRecipeResponse from "../../data/recipe/SingleRecipeResponse";
 
 export default interface ListSearchRecipeResponse {
+    totalCount: number;
+    totalPages: number;
+    currentPage: number;
+    pageSize: number;
     recipes: SingleRecipeResponse[];
 }
--- src/repository/integrated/IntegratedSearchRepository.ts
+++ src/repository/integrated/IntegratedSearchRepository.ts
@@ -11,11 +11,15 @@ export default class IntegratedSearchRepository {
 
     // 재료명 기반 통합 조회
     public async integratedIngredient(
+        page: number,
+        pageSize: number,
         term: string
     ): Promise<ListSearchRecipeResponse> {
         return this.httpRepository.get<ListSearchRecipeResponse>({
             path: "/seo/search/ingredient",
             params: {
+                page,
+                pageSize,
                 term,
             },
         });
@@ -23,11 +27,15 @@ export default class IntegratedSearchRepository {
 
     // cookingOrdeiList 기반 통합 조회
     public async integratedCookingOrderList(
+        page: number,
+        pageSize: number,
         term: string
     ): Promise<ListSearchRecipeResponse> {
         return this.httpRepository.get<ListSearchRecipeResponse>({
             path: "/seo/search/cookingorder",
             params: {
+                page,
+                pageSize,
                 term,
             },
         });
@@ -35,11 +43,15 @@ export default class IntegratedSearchRepository {
 
     // 레시피명 기반 통합 조회
     public async integratedRecipeName(
+        page: number,
+        pageSize: number,
         term: string
     ): Promise<ListSearchRecipeResponse> {
         return this.httpRepository.get<ListSearchRecipeResponse>({
             path: "/seo/search/recipename",
             params: {
+                page,
+                pageSize,
                 term,
             },
         });
