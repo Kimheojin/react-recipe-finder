286658787fdba0545f6aeca607242458707f63b4
Jin <hurjin1109@naver.com>
2025-10-31 17:38:39 +0900

    FEAT : tailwind 관련 설정 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,20 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, ".", "");
+    const env = loadEnv(mode, ".", "");
 
-  return {
-    plugins: [react()],
-    server: {
-      port: 1000,
-      proxy: {
-        "/seo": {
-          target: env.VITE_PROXY_TARGET,
-          changeOrigin: true,
+    return {
+        plugins: [react(), tailwindcss()],
+        server: {
+            port: 1000,
+            proxy: {
+                "/seo": {
+                    target: env.VITE_PROXY_TARGET,
+                    changeOrigin: true,
+                },
+            },
         },
-      },
-    },
-  };
+    };
 });
